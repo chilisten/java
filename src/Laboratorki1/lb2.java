5e05a9ec4a7e29b5804aea68e805c0f7b6483d2f
chilisten <dastankenzebekov099@gmail.com>
2024-06-10 20:15:01 +0600

    3 commit

---
+++ src/Laboratorki1/lb2.java
@@ -0,0 +1,31 @@
+package Laboratorki1;
+import java.util.*;
+
+public class lb2 {
+    public static String determineFinalState(List<String> actions) {
+        int likesCount = 0;
+        int dislikesCount = 0;
+
+        for (String action : actions) {
+            if (action.equals("Like")) {
+                likesCount++;
+            } else if (action.equals("Dislike")) {
+                dislikesCount++;
+            }
+        }
+
+        return (likesCount > dislikesCount) ? "Like" : (dislikesCount > likesCount) ? "Dislike" : "Nothing";
+    }
+
+    public static void main(String[] args) {
+        // Примеры тестов
+        List<String> test1 = List.of("Dislike");
+        System.out.println("Ввод: " + test1 + " ➞ Вывод: " + determineFinalState(test1));
+
+        List<String> test2 = List.of("Like", "Like");
+        System.out.println("Ввод: " + test2 + " ➞ Вывод: " + determineFinalState(test2));
+
+        List<String> test3 = List.of("Dislike", "Like");
+        System.out.println("Ввод: " + test3 + " ➞ Вывод: " + determineFinalState(test3));
+    }
+}
---
+++ src/Laboratorki1/lb3.java
@@ -0,0 +1,4 @@
+package Laboratorki1;
+
+public class lb3 {
+}
